e052e5a53c0138c18da3798bd5e8a8c463b7aa77
Andrzej Kurtycz <andrzejkurtycz@gmail.com>
2023-05-12 10:52:41 +0200

    variables, math, if, switch, arrow function

---
+++ assets/js/app.js
@@ -0,0 +1,228 @@
+// Excercise_1
+
+// // const imie = "Andrzej";
+// // console.log(imie);
+// // const wiek = 39;
+// // console.log(wiek);
+// // const ulubiona_potrawa = "bigos";
+// // console.log(ulubiona_potrawa);
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_2
+
+// const name = "Andrzej";
+// const nameDog = "Perys";
+// const ageDog = 10;
+
+// console.log(`Cześć jestem ${name}, a to jest ${nameDog} - mój pies`);
+// console.log(nameDog + " ma już " + ageDog + " lat");
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_3
+
+// const firstName = "Tomek";
+// let age = 12;
+// age = 18;
+
+// const favColor = "niebieski";
+// const favMeal = "schabowy";
+
+// let currentCar;
+// currentCar = "Audi";
+
+// let firstName2 = "Ania";
+// let age2 = 24;
+// let favColor2 = "czerwony";
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_4
+
+// const text1 = "powiększ mnie!";
+// console.log(text1.toUpperCase());
+
+// const text2 = "zapisz mnie małymi literami";
+// console.log(text2.toLowerCase());
+
+// const text3 = "@#$%% wytnij te dziwne zanki na początku!";
+// console.log(text3.slice(6));
+
+// const text4 = "sprawdź czy zawiera słowo czy";
+// console.log(text4.includes("czy"));
+
+// const text5 =
+//   'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj';
+// console.log(text5.charAt(2));
+
+// const text6 = "pies zamień każde słowo pies, na słowo kot pies";
+// console.log(text6.replaceAll("pies", "kot"));
+
+// const text7 = "podziel, ten, string, od, przecinków";
+// console.log(text7.split(","));
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_5
+
+// const pass = "kjagksaskdjkajsbkd";
+
+// if (pass.length > 10 && pass.includes("!")) {
+//   console.log("Masz rewalacyjne hasło");
+// } else if (pass.length > 10) {
+//   console.log("Masz dobre hasło");
+// } else {
+//   console.log("Masz za krótkie hasło");
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_6
+
+// const day = "sobota";
+
+// switch (day) {
+//   case "poniedziałek":
+//     console.log("dziś jest poniedziałek");
+//     break;
+
+//   case "wtorek":
+//     console.log("dziś jest wtorek");
+//     break;
+
+//   case "środa":
+//     console.log("dziś jest wtorek");
+//     break;
+
+//   case "czwartek":
+//     console.log("dziś jest wtorek");
+//     break;
+
+//   case "piątek":
+//     console.log("dziś jest wtorek");
+//     break;
+
+//   default:
+//     console.log("dziś jest weekend");
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_7
+
+// const x = 10;
+
+// if (x > 20) {
+//   console.log(x > 20);
+// } else {
+//   console.log(x < 20);
+// }
+
+// // Zwraca true lub false
+
+// const y = 10;
+
+// if (y > 20) {
+//   console.log(`${y} > 20`);
+// } else {
+//   console.log(`${y} < 20`);
+// }
+
+// Zwraca 10 < 20
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_8
+
+// const x = 10;
+
+// const newX = (x > 20) ? `${x} > 20` : `${x} < 20`;
+// console.log(newX);
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_9
+
+// const isLoggedIn = true;
+
+// function LoggedIn() {
+//   console.log("Użytkownik jest zalogowany!");
+// }
+
+// function LoggedOut() {
+//   console.log("Użytkownik nie jest zalogowany!");
+// }
+
+// isLoggedIn ? LoggedIn() : LoggedOut();
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_10
+
+// const x = 50;
+// const y = 30;
+
+// x > y ? console.log(`${x} > ${y}`) : "";
+
+// if (x > y) {
+//   console.log(`${x} > ${y}`);
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_11
+
+// const color = "blue";
+// newColor = "green";
+
+// if (color == newColor) {
+//   console.log("Kolory się zgadzają");
+// } else {
+//   console.log("Kolory się nie zgadzają");
+// }
+
+// color == newColor
+//   ? console.log("Kolory się zgadzają")
+//   : console.log("Kolory się nie zgadzają");
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_12
+
+// const x = 100;
+// const y = 50;
+
+// if (x > y) {
+//   console.log("x > y");
+// } else if (x == y) {
+//   console.log("x = y");
+// } else {
+//   console.log("x < y");
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_13
+
+// const promo = "20%";
+
+// switch (promo) {
+//   case "10%":
+//     console.log("Dziś mamy 10% zniżki");
+//     break;
+
+//   case "20%":
+//     console.log("Dziś mamy 20% zniżki");
+//     break;
+
+//   case "30%":
+//     console.log("Dziś mamy 30% zniżki");
+
+//   default:
+//     console.log("Dziś nie ma promocji");
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Excercise_14
